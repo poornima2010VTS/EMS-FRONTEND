549a61db4084fcd95f89039b33ac562e5ae87439
poornima suresh <poornima.teams@gmail.com>
2026-03-20 11:16:17 +0530

    fix API base URL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://127.0.0.1:8000/api/",
-   baseURL: "https://poornimar.pythonanywhere.com"
+   baseURL: "https://poornimar.pythonanywhere.com/api"
 });
 
 // ✅ ADD TOKEN AUTOMATICALLY
